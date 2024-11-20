d2195666aafa7496e52aa3f63ca1ec1238c53db8
Bartek Pers <bartekpers@Barteks-MacBook-Air.local>
2024-11-20 13:00:05 +0100

    fix ping

--- src/renderer/src/pages/Home.tsx
+++ src/renderer/src/pages/Home.tsx
@@ -1,7 +1,29 @@
 import Layout from '../components/Layout'
 import RedirectButton from '../components/RedirectButton'
+import axios from 'axios'
 
 export default function Home(): JSX.Element {
+  async function handlePing(): Promise<undefined> {
+    try {
+      console.log('Sent ping...')
+      const res = await axios.post('/ping', { data: 'ping' })
+      if (res.data?.data === 'pong') {
+        await window.electron.ipcRenderer.invoke('show-message-box', {
+          title: 'Connection test',
+          message: 'Pong',
+          buttons: ['OK']
+        })
+        console.log('Got pong')
+      }
+    } catch (err) {
+      console.log(err)
+      await window.electron.ipcRenderer.invoke('show-message-box', {
+        title: err.name,
+        message: err.toString(),
+        buttons: ['OK']
+      })
+    }
+  }
   return (
     <Layout>
       <div className="flex flex-col items-center gap-5">
@@ -11,6 +33,13 @@ export default function Home(): JSX.Element {
           <RedirectButton to="/commander">Commander</RedirectButton>
           <RedirectButton to="/shooter">Shooter</RedirectButton>
           <RedirectButton to="/reloader">Reloader</RedirectButton>
+        </div>
+
+        <div
+          className="p-2 cursor-pointer rounded-md bg-teal-300/60 hover:bg-teal-400/60"
+          onClick={handlePing}
+        >
+          Send ping
         </div>
       </div>
     </Layout>
