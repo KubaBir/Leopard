05e2d69548308c47e2ae77d5ddbee48f45e55dcb
Jakub Birecki <kubabir@outlook.com>
2024-11-27 11:11:55 +0100

    Feat: Cancel requests on mouse up

--- src/renderer/src/pages/Driver.tsx
+++ src/renderer/src/pages/Driver.tsx
@@ -1,16 +1,18 @@
 import Layout from '../components/Layout'
 import ActionButton from '../components/ActionButton'
 export default function Driver(): JSX.Element {
+  const fetchEndpoint = '/shooter'
+
   return (
     <Layout>
       <div className="flex flex-col items-center gap-5">
-        <ActionButton code={101} text="Up"></ActionButton>
+        <ActionButton fetchEndpoint={fetchEndpoint} code={101} text="Up"></ActionButton>
         <div className="flex w-96 justify-between ">
           {' '}
-          <ActionButton code={103} text="Left"></ActionButton>
-          <ActionButton code={104} text="Right"></ActionButton>
+          <ActionButton fetchEndpoint={fetchEndpoint} code={103} text="Left"></ActionButton>
+          <ActionButton fetchEndpoint={fetchEndpoint} code={104} text="Right"></ActionButton>
         </div>
-        <ActionButton code={102} text="Back"></ActionButton>
+        <ActionButton fetchEndpoint={fetchEndpoint} code={102} text="Back"></ActionButton>
       </div>
     </Layout>
   )
--- src/renderer/src/pages/Shooter.tsx
+++ src/renderer/src/pages/Shooter.tsx
@@ -1,9 +1,15 @@
+import ActionButton from '@renderer/components/ActionButton'
 import Layout from '../components/Layout'
 
 export default function Shooter(): JSX.Element {
+  const fetchEndpoint = '/shooter'
   return (
     <Layout>
       <h1>Shooter</h1>
+      <div className="flex flex-col items-center gap-5 p-4">
+        <ActionButton fetchEndpoint={fetchEndpoint} code={110} text="Cannon"></ActionButton>
+        <ActionButton fetchEndpoint={fetchEndpoint} code={111} text="Gun"></ActionButton>
+      </div>
     </Layout>
   )
 }
