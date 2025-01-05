9dd7f204c3b9a27052eebb0f516b7b6db288a9f0
Kuba Birecki <kubabir@outlook.com>
2025-01-05 12:25:28 +0100

    Feat: Header, commander view

--- src/renderer/src/components/Layout.tsx
+++ src/renderer/src/components/Layout.tsx
@@ -1,17 +1,35 @@
-import { ReactNode } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import RedirectButton from './RedirectButton'
+import { useLocation } from 'react-router-dom'
 
 interface LayoutProps {
   children: ReactNode
 }
 
 export default function Layout({ children }: LayoutProps): JSX.Element {
+  const [time, setTime] = useState(new Date())
+  const location = useLocation()
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return (): void => clearInterval(timer)
+  }, [])
+
   return (
-    <div className="w-screen h-screen bg-black flex justify-center items-center relative overflow-clip">
-      <div className="absolute top-0 left-0 ">
-        <RedirectButton to="/">Home</RedirectButton>
+    <div className="w-screen h-screen bg-black relative overflow-clip flex flex-col">
+      <div className="h-14 bg-gray-900 items-center justify-center grid grid-cols-3">
+        {location.pathname === '/' ? <div /> : <RedirectButton to="/">Home</RedirectButton>}
+        <div className="text-center text-xl leading-3">
+          {time.toLocaleTimeString('pl-PL', {
+            hour: 'numeric',
+            minute: 'numeric'
+          })}
+        </div>
       </div>
-      <div className="flex flex-col justify-center items-center w-full h-full">{children}</div>
+      <div className="flex flex-col justify-center items-center relative flex-1">{children}</div>
     </div>
   )
 }
