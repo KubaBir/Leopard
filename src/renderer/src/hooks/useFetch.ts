759b259858fe6c0d5626d12833e16c4d60a8a44f
b-pers <bartek.pers@gmail.com>
2025-01-10 09:53:03 +0100

    rewrite shooting logic

--- src/renderer/src/hooks/useFetch.ts
+++ src/renderer/src/hooks/useFetch.ts
@@ -1,13 +1,21 @@
 import axios from 'axios'
 
+export interface LoadedResponse {
+  isCanonLoaded: boolean
+  isGunLoaded: boolean
+}
+
 export const useFetch = (
-  args: { code: number; throttle?: number },
-  url?: string
-): { makeCall: () => Promise<string | void> } => {
+  args: { code?: number; throttle?: number } = {},
+  url?: string,
+  isPost: boolean = true
+): { makeCall: () => Promise<string | void | LoadedResponse> } => {
   const final_url = url ? url : '/request'
-  const makeCall = async (): Promise<string | void> => {
+  const makeCall = async (): Promise<string | void | LoadedResponse> => {
     try {
-      const response = await axios.post(final_url, args)
+      const response = isPost
+        ? await axios.post(final_url, args)
+        : await axios.get(final_url, { params: args })
       return response.data
     } catch (err) {
       await window.electron.ipcRenderer.invoke('show-message-box', {
